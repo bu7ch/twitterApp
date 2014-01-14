d82d8394c8c6caa3dc9eadb14689a790916d7ff2
bu7ch <dutchr@live.fr>
2014-01-14 14:14:44 +0100

    deploy on github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
 
-before_validation :prep_email
-before_save :create_avatar_url
+	before_validation :prep_email
+	before_save :create_avatar_url
 
-attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
-has_secure_password
-validates :name, presence: true
-validates :username, uniqueness: true, presence: true
-validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+	has_secure_password
+	validates :name, presence: true
+	validates :username, uniqueness: true, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+end
 
 def create_avatar_url
     self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
@@ -18,3 +21,4 @@ private
 def prep_email
     self.email = self.email.strip.downcase if self.email
 end
+
